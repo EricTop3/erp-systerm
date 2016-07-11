ae9f9fd2dc5d3198daf3f9634612e8ec502a679c
tzq <tangzq06@163.com>
2016-07-11 10:15:55 +0800

    会员页面完成

--- src/publicFunction/index.js
+++ src/publicFunction/index.js
@@ -1,4 +1,4 @@
 // 请求服务器路径
-export var requestUrl = 'http://115.28.216.235:1401/mock/v1'
+export var requestUrl = 'http://192.168.1.150:1401/mock/v1'
 // token值
 export var token = window.localStorage.getItem('token')
