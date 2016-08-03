e28028370930b17d8112b51e2b0d447b8f1e8735
tzq <tangzq06@163.com>
2016-08-03 09:52:42 +0800

    生产左侧导航修改

--- src/components/admin/common/LeftProduction.vue
+++ src/components/admin/common/LeftProduction.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="col-lg-2" role="navigation">
     <ul class="nav nav-stacked sidebar">
-      <li><a v-link="{ path: '/admin/production'}">工厂生产单</a></li>
-      <li><a v-link="{ path: '/admin/production/delegationCreat'}">委外生产单</a></li>
-      <li><a v-link="{ path: '/admin/production/getSupplies'}">领料单</a></li>
-      <li><a v-link="{ path: '/admin/production/factoryInstock'}">生产入库单</a></li>
-      <li><a v-link="{ path: '/admin/production/delegationInstock'}">委外生产入库单</a></li>
-      <li><a v-link="{ path: '/admin/production/creatOrder'}">生产预约单</a></li>
+      <li><a v-link="{ path: '/admin/production',exact: true}">工厂生产单</a></li>
+      <li><a v-link="{ path: '/admin/production/delegationCreat',exact: true}">委外生产单</a></li>
+      <li><a v-link="{ path: '/admin/production/getSupplies',exact: true}">领料单</a></li>
+      <li><a v-link="{ path: '/admin/production/factoryInstock',exact: true}">工厂生产入库单</a></li>
+      <li><a v-link="{ path: '/admin/production/delegationInstock',exact: true}">委外生产入库单</a></li>
+      <li><a v-link="{ path: '/admin/production/creatOrder',exact: true}">生产预约单</a></li>
     </ul>
   </div>
 </template>
