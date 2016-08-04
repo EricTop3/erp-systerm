fd3ca1f334c397efd661d0e3e642f2c252c050b5
tzq <tangzq06@163.com>
2016-08-04 20:09:46 +0800

    库存盘点列表

--- src/components/admin/instock/inventoryStock/AdminInstockInventory.vue
+++ src/components/admin/instock/inventoryStock/AdminInstockInventory.vue
@@ -16,167 +16,199 @@
           <form class="form-inline">
             <div class="form-group">
               <label>仓库</label>
-              <select class="form-control">
-                <option>水星店</option>
+              <select class="form-control" v-model="search.selectedHouse">
+                <option value="">请选择</option>
+                <option :value="item.id" v-for="item in search.warehouseList">{{item.name}}</option>
               </select>
             </div>
             <div class="form-group ml10">
               <label>单号</label>
-              <input type="text" class="form-control" placeholder="">
+              <input type="text" class="form-control" placeholder="请输入单号" v-model="search.code">
             </div>
             <div class="form-group ml10">
               <label>审核状态</label>
-              <select class="form-control">
-                <option>请选择</option>
+              <select class="form-control" v-model="search.selectedStatus">
+                <option value="">请选择</option>
+                <option value="0">未审核</option>
+                <option value="1">已审核</option>
               </select>
             </div>
             <div class="form-group ml10">
               <label>制单人</label>
-              <select class="form-control">
-                <option>请选择</option>
+              <select class="form-control" v-model="search.selectedMaker">
+                <option value="">请选择</option>
+                <option :value="item.id" v-for="item in search.orderMaker">{{item.name}}</option>
               </select>
             </div>
-            <div class="form-group ml10">
+            <br>
+            <div class="form-group ml10 mt20">
               <label>盘点时间段</label>
-              <input type="text"class="form-control date_picker" placeholder="开始时间"> -
-              <input type="text"class="form-control date_picker" placeholder="结束时间">
+              <date-picker :value.sync="search.startTime"></date-picker>
+              -
+              <date-picker :value.sync="search.endTime"></date-picker>
             </div>
-            <button type="submit" class="btn btn-primary">搜索</button>
-            <span class="btn btn-warning">撤销搜索</span>
+
+            <button class="btn btn-primary mt20" @click="searchMethod">搜索</button>
+            <span class="btn btn-warning mt20" @click="cancelSearch">撤销搜索</span>
 
             <span class="btn fr btn-info">导出excel</span>
-            <span v-link="{ path:'/admin/instock/createInventory' }" class="btn btn-info spanblocks fr mr10">新建盘点单</span>
+            <span v-link="{ path:'/admin/instock/createInventory' }"
+                  class="btn btn-info spanblocks fr mr10">新建盘点单</span>
           </form>
         </div>
 
         <!-- 表格 -->
-        <table class="table table-striped table-border table-hover">
-          <thead>
-          <tr class="text-center">
-            <td class="text-left">盘点单号</td>
-            <td>审核状态</td>
-            <td>仓库</td>
-            <td>盘点日期</td>
-            <td>差异库存量</td>
-            <td>制单人</td>
-            <td>审核人</td>
-            <td>操作</td>
-          </tr>
-          </thead>
-          <tbody>
-          <tr class="text-center">
-            <td class="text-left">1654679184236564</td>
-            <td>未审核</td>
-            <td>总部仓库</td>
-            <td>2016-6-1</td>
-            <td>-5</td>
-            <td>张三</td>
-            <td>无</td>
-            <td>
-              <span class="btn btn-primary btn-sm" data-toggle="modal" data-target="#inventory-audit-templ">审核</span>
-              <span class="btn btn-default btn-sm" data-toggle="modal" data-target="#inventory-view-templ">查看</span>
-              <span class="btn btn-default btn-sm" data-toggle="modal" data-target="#inventory-del-templ">删除</span>
-            </td>
-          </tr>
-          <tr class="text-center">
-            <td class="text-left">1654679184236564</td>
-            <td>已审核</td>
-            <td>总部仓库</td>
-            <td>2016-6-1</td>
-            <td>-5</td>
-            <td>张三</td>
-            <td>江小白</td>
-            <td>
-              <span class="btn btn-default btn-sm" data-toggle="modal" data-target="#inventory-view-templ">查看</span>
-            </td>
-          </tr>
-          </tbody>
-        </table>
-        <!-- 翻页 -->
-        <nav class="text-right">
-          <ul class="pagination">
-            <li><a href="#" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>
-            <li class="active"><a href="#">1</a></li>
-            <li><a href="#">2</a></li>
-            <li><a href="#">3</a></li>
-            <li><a href="#">4</a></li>
-            <li><a href="#">5</a></li>
-            <li><a href="#" aria-label="Next"><span aria-hidden="true">&raquo;</span></a></li>
-          </ul>
-        </nav>
+        <summary
+          :table-data="list"
+          :table-header="gridColumns"
+          :page="page"
+          :check-url="checkUrl"
+          :finish-url="checkUrl"
+          :finish-flag="true"
+        >
+        </summary>
 
       </div>
     </div>
   </div>
-
-  <!--模态框-审核-->
-  <div class="modal fade" id="inventory-audit-templ" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-    <div class="modal-dialog modal-sm" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-          <h4 class="modal-title">审核</h4>
-        </div>
-        <div class="modal-body">
-          <h4>审核弹出</h4>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!--模态框HTML-->
-
-  <!--模态框-查看-->
-  <div class="modal fade" id="inventory-view-templ" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-    <div class="modal-dialog modal-sm" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-          <h4 class="modal-title">查看</h4>
-        </div>
-        <div class="modal-body">
-          <h4>查看弹出</h4>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>
-          <button type="button" class="btn btn-primary">保存</button>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!--模态框HTML-->
-
-  <!--模态框-删除-->
-  <div class="modal fade" id="inventory-del-templ" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-    <div class="modal-dialog modal-sm" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-          <h4 class="modal-title">删除</h4>
-        </div>
-        <div class="modal-body">
-          <h4>删除弹出</h4>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>
-          <button type="button" class="btn btn-primary">保存</button>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!--模态框HTML-->
 </template>
 <style>
 </style>
 <script>
-  import LeftInstock from '../../common/LeftInstock'
+  import $ from 'jquery'
+  import Grid from '../../../common/Grid'
+  import Page from '../../../common/Page'
   import AdminNav from '../../AdminNav'
+  import Modal from '../../../common/Modal'
+  import Summary from '../../../common/Summary'
+  import DatePicker from  '../../../common/DatePicker'
+  import LeftInstock from '../../common/LeftInstock'
+  import {
+    requestUrl,
+    requestSystemUrl,
+    getDataFromApi,
+    token,
+    exchangeData,
+    searchRequest,
+    deleteRequest,
+    checkRequest,
+    finishRequest,
+    changeStatus
+  } from '../../../../publicFunction/index'
   export default{
     components: {
       LeftInstock: LeftInstock,
-      AdminNav: AdminNav
+      Grid: Grid,
+      Page: Page,
+      Modal: Modal,
+      AdminNav: AdminNav,
+      Summary: Summary,
+      DatePicker: DatePicker
+    },
+    ready: function () {
+      var self = this
+//    获取仓库列表
+      var url = requestSystemUrl + '/backend-system/warehouse-minimal-list'
+      getDataFromApi(url, {}, function (response) {
+        self.search.warehouseList = response.data.body.list
+      })
+//    获取制单人
+      var makUrl = requestSystemUrl + '/backend-system/store/store-account'
+      getDataFromApi(makUrl, {}, function (response) {
+        self.search.orderMaker = response.data.body.list
+      })
+
+      this.listData({})
+    },
+    events: {
+//    绑定翻页事件
+      pagechange: function (currentpage) {
+        var self = this
+        this.$http({
+          url: requestSystemUrl + '/backend-system/stock/inventory',
+          data: {
+            page: currentpage
+          },
+          method: 'get',
+          headers: {'X-Overpowered-Token': token}
+        }).then(function (response) {
+          self.page = response.data.body.pagination
+          self.list = response.data.body.list
+          changeStatus(self.list)
+        }, function (err) {
+          console.log(err)
+        })
+      },
+//     删除请求
+      deleteFromApi: function (id) {
+        var self = this
+        deleteRequest(requestSystemUrl + '/backend-system/stock/inventory/' + id, function (response) {
+          self.listData({})
+        })
+      },
+//    查看详情
+      gotoDetail: function (id) {
+//        window.location.href = '#!/admin/purchase/order/purchasedetail/'+ id
+      }
+    },
+    methods: {
+      listData: function (data) {
+        var self = this
+        var url = requestSystemUrl + '/backend-system/stock/inventory'
+        getDataFromApi(url, data, function (response) {
+          self.list = response.data.body.list
+          self.page = response.data.body.pagination
+          changeStatus(self.list)
+        })
+      },
+//      搜索
+      searchMethod: function () {
+        var data = {
+          start_time: this.search.start_time,
+          end_time: this.search.end_time,
+          document_number: this.search.code,
+          creator_id: this.search.selectedMaker,
+          warehouse_id: this.search.selectedHouse,
+          checked: this.search.selectedStatus
+        }
+        this.listData(data)
+      },
+//      撤销搜索
+      cancelSearch: function () {
+        this.search.start_time = ''
+        this.search.end_time = ''
+        this.search.code = ''
+        this.search.selectedMaker = ''
+        this.search.selectedHouse = ''
+        this.search.selectedStatus = ''
+        this.listData({})
+      }
+    },
+    data: function () {
+      return {
+        page: [],
+        list: [],
+        checkUrl: requestSystemUrl + '/backend-system/stock/inventory/',
+        gridColumns: {
+          document_number: '盘点单号',
+          checked: '审核状态',
+          warehouse_name: '仓库',
+          created_at: '盘点日期',
+          difference: '差异库存量',
+          creator_name: '制单人',
+          auditor_name: '审核人'
+        },
+        search: {
+          orderMaker: [],
+          warehouseList: [],
+          selectedHouse: '',
+          code: '',
+          selectedStatus: '',
+          selectedMaker: '',
+          startTime: '',
+          endTime: ''
+        }
+      }
     }
   }
 </script>
